73dab6cf55bdae8c1c3b447ed90eae31c0c06753
Andrew Radev <andrey.radev@gmail.com>
2021-12-26 11:34:28 +0200

    Filter filenames, minor cleanup

--- src/app.rs
+++ src/app.rs
@@ -2,12 +2,12 @@ use std::path::PathBuf;
 
 use eframe::{egui, epi};
 
-/// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[cfg_attr(feature = "persistence", derive(serde::Deserialize, serde::Serialize))]
 #[cfg_attr(feature = "persistence", serde(default))] // if we add new fields, give them default values when deserializing old state
 pub struct Mp3sApp {
     music_root: String,
     selected_path: Option<PathBuf>,
+    filter: String,
 }
 
 impl Default for Mp3sApp {
@@ -17,7 +17,7 @@ impl Default for Mp3sApp {
             unwrap_or_else(|| ::std::env::current_dir().unwrap());
         let music_root = format!("{}", music_root.display());
 
-        Self { music_root, selected_path: None::<PathBuf> }
+        Self { music_root, selected_path: None::<PathBuf>, filter: String::new() }
     }
 }
 
@@ -26,36 +26,24 @@ impl epi::App for Mp3sApp {
         "Basic Mp3 Viewer"
     }
 
-    /// Called once before the first frame.
     fn setup(
         &mut self,
         _ctx: &egui::CtxRef,
         _frame: &mut epi::Frame<'_>,
         _storage: Option<&dyn epi::Storage>,
     ) {
-        // Load previous app state (if any).
-        // Note that you must enable the `persistence` feature for this to work.
         #[cfg(feature = "persistence")]
         if let Some(storage) = _storage {
             *self = epi::get_value(storage, epi::APP_KEY).unwrap_or_default()
         }
     }
 
-    /// Called by the frame work to save state before shutdown.
-    /// Note that you must enable the `persistence` feature for this to work.
     #[cfg(feature = "persistence")]
     fn save(&mut self, storage: &mut dyn epi::Storage) {
         epi::set_value(storage, epi::APP_KEY, self);
     }
 
-    /// Called each time the UI needs repainting, which may be many times per second.
-    /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::CtxRef, frame: &mut epi::Frame<'_>) {
-        // Examples of how to create different panels and windows.
-        // Pick whichever suits you.
-        // Tip: a good default choice is to just keep the `CentralPanel`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
-
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             // The top panel is often a good place for a menu bar:
             egui::menu::bar(ui, |ui| {
@@ -101,21 +89,31 @@ impl epi::App for Mp3sApp {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("List of mp3s");
 
+            ui.label("Filter: ");
+            ui.text_edit_singleline(&mut self.filter);
+
             egui::ScrollArea::vertical().show(ui, |ui| {
                 for entry in std::fs::read_dir(&self.music_root).unwrap() {
                     let entry = entry.unwrap();
                     let path = entry.path();
 
-                    if entry.path().extension() == Some("mp3".as_ref()) {
-                        let filename = path.strip_prefix(&self.music_root).unwrap();
-                        let selected = Some(filename) == self.selected_path.as_ref().map(|p| p.as_path());
+                    if path.extension() != Some("mp3".as_ref()) {
+                        continue;
+                    }
 
-                        if ui.selectable_label(selected, format!("{}", filename.display())).clicked() {
-                            if selected {
-                                self.selected_path = None;
-                            } else {
-                                self.selected_path = Some(filename.to_owned());
-                            }
+                    let filename = path.strip_prefix(&self.music_root).unwrap();
+                    if self.filter.trim() != "" &&
+                        !filename.to_string_lossy().to_lowercase().contains(&self.filter.to_lowercase()) {
+                        continue;
+                    }
+
+                    let selected = Some(filename) == self.selected_path.as_ref().map(|p| p.as_path());
+
+                    if ui.selectable_label(selected, format!("{}", filename.display())).clicked() {
+                        if selected {
+                            self.selected_path = None;
+                        } else {
+                            self.selected_path = Some(filename.to_owned());
                         }
                     }
                 }
@@ -125,15 +123,5 @@ impl epi::App for Mp3sApp {
         egui::TopBottomPanel::bottom("bottom_panel").show(ctx, |ui| {
             egui::warn_if_debug_build(ui);
         });
-
-        // TODO: Open window on clicking something -- Tag editing?
-        if false {
-            egui::Window::new("Window").show(ctx, |ui| {
-                ui.label("Windows can be moved by dragging them.");
-                ui.label("They are automatically sized based on contents.");
-                ui.label("You can turn on resizing and scrolling if you like.");
-                ui.label("You would normally chose either panels OR windows.");
-            });
-        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,3 @@
-#![forbid(unsafe_code)]
-#![cfg_attr(not(debug_assertions), deny(warnings))] // Forbid warnings in release builds
-#![warn(clippy::all, rust_2018_idioms)]
-
 fn main() {
     let app = egui_mp3s::Mp3sApp::default();
     let native_options = eframe::NativeOptions::default();
